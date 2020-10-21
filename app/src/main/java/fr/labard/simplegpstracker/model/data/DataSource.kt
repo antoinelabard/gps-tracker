273bb793d8128921b24dee6612552d7cccda1575
Antoine Labard <antoine@labard.fr>
2020-10-21 14:09:28 +0200

    Provided a basic implementation of LocalDataSource.kt

---
+++ app/src/main/java/fr/labard/simplegpstracker/model/data/DataSource.kt
@@ -0,0 +1,18 @@
+package fr.labard.simplegpstracker.model.data
+
+import androidx.lifecycle.LiveData
+import fr.labard.simplegpstracker.model.data.local.db.location.LocationEntity
+import fr.labard.simplegpstracker.model.data.local.db.record.RecordEntity
+
+interface DataSource {
+    fun getRecords(): LiveData<List<RecordEntity>>
+    fun insertRecord(recordEntity: RecordEntity)
+    fun updateRecordName(id: Int, name: String)
+    fun updateLastRecordModification(id: Int)
+    fun deleteRecord(recordId: Int)
+
+    fun getLocations(): LiveData<List<LocationEntity>>
+    fun insertLocation(locationEntity: LocationEntity)
+
+    fun deleteAll()
+}
---
+++ app/src/main/java/fr/labard/simplegpstracker/model/data/local/LocalDataSource.kt
@@ -0,0 +1,54 @@
+package fr.labard.simplegpstracker.model.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ServiceLifecycleDispatcher
+import fr.labard.simplegpstracker.model.data.DataSource
+import fr.labard.simplegpstracker.model.data.Resource
+import fr.labard.simplegpstracker.model.data.local.db.location.LocationDao
+import fr.labard.simplegpstracker.model.data.local.db.location.LocationEntity
+import fr.labard.simplegpstracker.model.data.local.db.record.RecordDao
+import fr.labard.simplegpstracker.model.data.local.db.record.RecordEntity
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import java.lang.Error
+import java.util.*
+
+class LocalDataSource(
+    private val recordDao: RecordDao,
+    private val locationDao: LocationDao,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+): DataSource {
+
+    override fun getRecords(): LiveData<List<RecordEntity>> {
+        return recordDao.getAll()
+    }
+
+    override fun insertRecord(recordEntity: RecordEntity) {
+        recordDao.insert(recordEntity)
+    }
+
+    override fun updateRecordName(id: Int, name: String) {
+        recordDao.updateName(id, name)
+    }
+
+    override fun updateLastRecordModification(id: Int) {
+        recordDao.updateLastRecordModification(id, Date())
+    }
+
+    override fun deleteRecord(recordId: Int) {
+        recordDao.deleteById(recordId)
+    }
+
+    override fun getLocations(): LiveData<List<LocationEntity>> {
+        return locationDao.getAll()
+    }
+
+    override fun insertLocation(locationEntity: LocationEntity) {
+        locationDao.insert(locationEntity)
+    }
+
+    override fun deleteAll() {
+        recordDao.deleteAll()
+        locationDao.deleteAll()
+    }
+}
