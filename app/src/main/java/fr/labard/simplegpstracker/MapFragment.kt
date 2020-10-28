e690b61d49914061e1a68ac24abfa21fc24f2d39
Antoine Labard <antoine@labard.fr>
2020-10-28 08:42:50 +0100

    Added a fragment for the map.

---
+++ app/src/main/java/fr/labard/simplegpstracker/MapFragment.kt
@@ -0,0 +1,31 @@
+package fr.labard.simplegpstracker
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MapFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MapFragment()
+    }
+
+    private lateinit var viewModel: MapFragmentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MapFragmentViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/fr/labard/simplegpstracker/MapFragmentViewModel.kt
@@ -0,0 +1,7 @@
+package fr.labard.simplegpstracker
+
+import androidx.lifecycle.ViewModel
+
+class MapFragmentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
