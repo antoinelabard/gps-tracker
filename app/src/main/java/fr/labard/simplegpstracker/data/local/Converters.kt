cadcff491598f12be850484c5a9206524cf6e80a
Antoine Labard <antoine@labard.fr>
2020-11-18 14:51:14 +0100

    wrote documentation for Converters.kt.

--- app/src/main/java/fr/labard/simplegpstracker/data/local/Converters.kt
+++ app/src/main/java/fr/labard/simplegpstracker/data/local/Converters.kt
@@ -3,12 +3,28 @@ package fr.labard.simplegpstracker.data.local
 import androidx.room.TypeConverter
 import java.util.*
 
+/**
+ * Convert the java type Date to Long to make it compatible with the database.
+ */
 class Converters {
+
+    /**
+     * Return the date associated with the timestamp.
+     *
+     * @param timestamp the timestamp stored in the database
+     * @return the date associated with this timestamp provided
+     */
     @TypeConverter
-    fun timestampToDate(value: Long?): Date? {
-        return value?.let { Date(it) }
+    fun timestampToDate(timestamp: Long?): Date? {
+        return timestamp?.let { Date(it) }
     }
 
+    /**
+     * Return the timestamp associated with the date.
+     *
+     * @param date the date
+     * @return the to be stored in the database
+     */
     @TypeConverter
     fun dateToTimestamp(date: Date?): Long? {
         return date?.time // in milliseconds
