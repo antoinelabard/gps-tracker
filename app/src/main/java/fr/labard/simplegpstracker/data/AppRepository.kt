d2d2fc9dc65770305fcbf4f5170d1760116ed458
Antoine Labard <antoine@labard.fr>
2020-11-18 11:24:08 +0100

    Added some components to manage idling resources and data binding for the tests.

--- app/src/main/java/fr/labard/simplegpstracker/data/AppRepository.kt
+++ app/src/main/java/fr/labard/simplegpstracker/data/AppRepository.kt
@@ -2,6 +2,7 @@ package fr.labard.simplegpstracker.data
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import fr.labard.simplegpstracker.EspressoIdlingResource.wrapEspressoIdlingResource
 import fr.labard.simplegpstracker.data.local.LocationEntity
 import fr.labard.simplegpstracker.data.local.RecordEntity
 
@@ -13,34 +14,50 @@ class AppRepository internal constructor(
     override var activeRecordId = MutableLiveData("")
 
     override fun getRecords(): LiveData<List<RecordEntity>> {
-        return localDataSource.getRecords()
+        wrapEspressoIdlingResource {
+            return localDataSource.getRecords()
+        }
     }
 
     override fun insertRecord(recordEntity: RecordEntity) {
-        localDataSource.insertRecord(recordEntity)
+        wrapEspressoIdlingResource {
+            localDataSource.insertRecord(recordEntity)
+        }
     }
 
     override fun updateRecordName(id: String, name: String) {
-        localDataSource.updateRecordName(id, name)
+        wrapEspressoIdlingResource {
+            localDataSource.updateRecordName(id, name)
+        }
     }
 
     override fun updateLastRecordModification(id: String) {
-        localDataSource.updateLastRecordModification(id)
+        wrapEspressoIdlingResource {
+            localDataSource.updateLastRecordModification(id)
+        }
     }
 
     override fun deleteRecord(id: String) {
-        localDataSource.deleteRecord(id)
+        wrapEspressoIdlingResource {
+            localDataSource.deleteRecord(id)
+        }
     }
 
     override fun getLocations(): LiveData<List<LocationEntity>> {
-        return localDataSource.getLocations()
+        wrapEspressoIdlingResource {
+            return localDataSource.getLocations()
+        }
     }
 
     override fun insertLocation(locationEntity: LocationEntity) {
-        localDataSource.insertLocation(locationEntity)
+        wrapEspressoIdlingResource {
+            localDataSource.insertLocation(locationEntity)
+        }
     }
 
     override fun deleteAll() {
-        localDataSource.deleteAll()
+        wrapEspressoIdlingResource {
+            localDataSource.deleteAll()
+        }
     }
 }
