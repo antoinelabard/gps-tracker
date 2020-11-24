633488254e3c889accd2570d24dc0e59e0060669
Antoine Labard <antoine@labard.fr>
2020-11-24 14:29:22 +0100

    Commented a non essential portion of testing code which prevented from generating the signed apk (I don't know why yet).

--- app/src/main/java/fr/labard/simplegpstracker/util/DataBindingIdlingResource.kt
+++ app/src/main/java/fr/labard/simplegpstracker/util/DataBindingIdlingResource.kt
@@ -3,9 +3,7 @@ package fr.labard.simplegpstracker.util
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.testing.FragmentScenario
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.IdlingResource
 import java.util.*
@@ -76,8 +74,8 @@ fun DataBindingIdlingResource.monitorActivity(
 /**
  * Sets the fragment from a [FragmentScenario] to be used from [DataBindingIdlingResource].
  */
-fun DataBindingIdlingResource.monitorFragment(fragmentScenario: FragmentScenario<out Fragment>) {
-    fragmentScenario.onFragment {
-        this.activity = it.requireActivity()
-    }
-}
+//fun DataBindingIdlingResource.monitorFragment(fragmentScenario: FragmentScenario<out Fragment>) {
+//    fragmentScenario.onFragment {
+//        this.activity = it.requireActivity()
+//    }
+//}
