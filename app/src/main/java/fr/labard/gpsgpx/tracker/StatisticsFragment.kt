936c148e6a7a3feb4c637b0cc2d5693e80615e1d
Antoine Labard <antoine@labard.fr>
2021-02-17 18:04:31 +0100

    Implemented the data binding for the statistics fragment. (partially)

--- app/src/main/java/fr/labard/gpsgpx/tracker/StatisticsFragment.kt
+++ app/src/main/java/fr/labard/gpsgpx/tracker/StatisticsFragment.kt
@@ -9,10 +9,13 @@ import android.os.IBinder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import fr.labard.gpsgpx.GPSApplication
 import fr.labard.gpsgpx.R
+import fr.labard.gpsgpx.databinding.FragmentRecordBinding
+import fr.labard.gpsgpx.databinding.FragmentStatisticsBinding
 import fr.labard.gpsgpx.util.StatisticsPresenter
 import kotlinx.android.synthetic.main.fragment_statistics.*
 
@@ -46,8 +49,12 @@ class StatisticsFragment : DialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_statistics, container, false)
+    ): View {
+        val binding: FragmentStatisticsBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_statistics, container, false)
+        binding.lifecycleOwner = this
+        binding.viewmodel = viewModel
+        val view = binding.root
         dialog?.setTitle(getString(R.string.statistics_title))
 
         viewModel.allLocations.observe(viewLifecycleOwner, {
@@ -69,30 +76,6 @@ class StatisticsFragment : DialogFragment() {
         super.onStop()
         activity?.unbindService(connection)
         viewModel.serviceIsBound = false
-    }
-
-    fun updateStatisticsDisplay() {
-        fragment_statistics_textview_total_distance.text = getString(R.string.stats_total_distance).format(
-            StatisticsPresenter.getTotalDistanceFormatted(viewModel.locationsByRecordId)
-        )
-        fragment_statistics_textview_total_time.text = getString(R.string.stats_total_time).format(
-            StatisticsPresenter.getTotalTimeFormatted(viewModel.locationsByRecordId)
-        )
-        fragment_statistics_textview_recent_speed.text = getString(R.string.stats_recent_speed).format(
-            StatisticsPresenter.getRecentSPeedFormatted(viewModel.locationsByRecordId)
-        )
-        fragment_statistics_textview_average_speed.text = getString(R.string.stats_average_speed).format(
-            StatisticsPresenter.getAverageSpeedFormatted(viewModel.locationsByRecordId)
-        )
-        fragment_statistics_textview_min_speed.text = getString(R.string.stats_min_speed).format(
-            StatisticsPresenter.getMinSpeedFormatted(viewModel.locationsByRecordId)
-        )
-        fragment_statistics_textview_max_speed.text = getString(R.string.stats_max_speed).format(
-            StatisticsPresenter.getMaxSpeedFormatted(viewModel.locationsByRecordId)
-        )
-        fragment_statistics_textview_nb_locations.text = getString(R.string.stats_nb_locations).format(
-            StatisticsPresenter.getNbLocationsPresented(viewModel.locationsByRecordId)
-        )
     }
 
     companion object {
--- app/src/main/java/fr/labard/gpsgpx/tracker/StatisticsFragmentViewModel.kt
+++ app/src/main/java/fr/labard/gpsgpx/tracker/StatisticsFragmentViewModel.kt
@@ -2,8 +2,10 @@ package fr.labard.gpsgpx.tracker
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import fr.labard.gpsgpx.R
 import fr.labard.gpsgpx.data.IRepository
 import fr.labard.gpsgpx.data.local.LocationEntity
+import fr.labard.gpsgpx.util.StatisticsPresenter
 
 class StatisticsFragmentViewModel(
     appRepository: IRepository
@@ -16,6 +18,30 @@ class StatisticsFragmentViewModel(
     fun setLocationsByActiveRecordId() {
         activeRecordId?.let { id ->
             locationsByRecordId = allLocations.value?.filter { it.recordId == id } ?: listOf() }
+    }
+
+    fun updateStatisticsDisplay() {
+        fragment_statistics_textview_total_distance.text = getString(R.string.stats_total_distance).format(
+            StatisticsPresenter.getTotalDistanceFormatted(viewModel.locationsByRecordId)
+        )
+        fragment_statistics_textview_total_time.text = getString(R.string.stats_total_time).format(
+            StatisticsPresenter.getTotalTimeFormatted(viewModel.locationsByRecordId)
+        )
+        fragment_statistics_textview_recent_speed.text = getString(R.string.stats_recent_speed).format(
+            StatisticsPresenter.getRecentSPeedFormatted(viewModel.locationsByRecordId)
+        )
+        fragment_statistics_textview_average_speed.text = getString(R.string.stats_average_speed).format(
+            StatisticsPresenter.getAverageSpeedFormatted(viewModel.locationsByRecordId)
+        )
+        fragment_statistics_textview_min_speed.text = getString(R.string.stats_min_speed).format(
+            StatisticsPresenter.getMinSpeedFormatted(viewModel.locationsByRecordId)
+        )
+        fragment_statistics_textview_max_speed.text = getString(R.string.stats_max_speed).format(
+            StatisticsPresenter.getMaxSpeedFormatted(viewModel.locationsByRecordId)
+        )
+        fragment_statistics_textview_nb_locations.text = getString(R.string.stats_nb_locations).format(
+            StatisticsPresenter.getNbLocationsPresented(viewModel.locationsByRecordId)
+        )
     }
 }
 
