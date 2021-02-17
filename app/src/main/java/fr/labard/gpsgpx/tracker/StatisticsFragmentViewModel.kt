7465de2da5488352c396fd04a244ff82b5c8923c
Antoine Labard <antoine@labard.fr>
2021-02-17 18:04:31 +0100

    Implemented the data binding for the statistics fragment. (partially)

--- app/src/main/java/fr/labard/gpsgpx/tracker/StatisticsFragmentViewModel.kt
+++ app/src/main/java/fr/labard/gpsgpx/tracker/StatisticsFragmentViewModel.kt
@@ -1,5 +1,6 @@
 package fr.labard.gpsgpx.tracker
 
+import android.provider.Settings.Global.getString
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import fr.labard.gpsgpx.R
@@ -15,33 +16,28 @@ class StatisticsFragmentViewModel(
     var locationsByRecordId = listOf<LocationEntity>()
     var serviceIsBound = false
 
+    var stats = Statistics()
+
+    inner class Statistics {
+        val totalDistance
+            get() = StatisticsPresenter.getTotalDistanceFormatted(locationsByRecordId)
+        val totalTime
+            get() = StatisticsPresenter.getTotalTimeFormatted(locationsByRecordId)
+        val recentSpeed
+        get() = StatisticsPresenter.getRecentSPeedFormatted(locationsByRecordId)
+        val averageSpeed
+            get() = StatisticsPresenter.getAverageSpeedFormatted(locationsByRecordId)
+        val minSpeed
+            get() = StatisticsPresenter.getMinSpeedFormatted(locationsByRecordId)
+        val maxSpeed
+            get() = StatisticsPresenter.getMaxSpeedFormatted(locationsByRecordId)
+        val nbLocations
+            get() = StatisticsPresenter.getNbLocationsPresented(locationsByRecordId)
+    }
+
     fun setLocationsByActiveRecordId() {
         activeRecordId?.let { id ->
             locationsByRecordId = allLocations.value?.filter { it.recordId == id } ?: listOf() }
-    }
-
-    fun updateStatisticsDisplay() {
-        fragment_statistics_textview_total_distance.text = getString(R.string.stats_total_distance).format(
-            StatisticsPresenter.getTotalDistanceFormatted(viewModel.locationsByRecordId)
-        )
-        fragment_statistics_textview_total_time.text = getString(R.string.stats_total_time).format(
-            StatisticsPresenter.getTotalTimeFormatted(viewModel.locationsByRecordId)
-        )
-        fragment_statistics_textview_recent_speed.text = getString(R.string.stats_recent_speed).format(
-            StatisticsPresenter.getRecentSPeedFormatted(viewModel.locationsByRecordId)
-        )
-        fragment_statistics_textview_average_speed.text = getString(R.string.stats_average_speed).format(
-            StatisticsPresenter.getAverageSpeedFormatted(viewModel.locationsByRecordId)
-        )
-        fragment_statistics_textview_min_speed.text = getString(R.string.stats_min_speed).format(
-            StatisticsPresenter.getMinSpeedFormatted(viewModel.locationsByRecordId)
-        )
-        fragment_statistics_textview_max_speed.text = getString(R.string.stats_max_speed).format(
-            StatisticsPresenter.getMaxSpeedFormatted(viewModel.locationsByRecordId)
-        )
-        fragment_statistics_textview_nb_locations.text = getString(R.string.stats_nb_locations).format(
-            StatisticsPresenter.getNbLocationsPresented(viewModel.locationsByRecordId)
-        )
     }
 }
 
