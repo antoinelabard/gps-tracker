5b09d77926e937af9bb871f4ae0e6cf04b2da374
Antoine Labard <antoine@labard.fr>
2021-03-03 17:53:39 +0100

    Added a binding adapter for the record fragment.

--- app/src/main/java/fr/labard/gpsgpx/util/BindingAdapters.kt
+++ app/src/main/java/fr/labard/gpsgpx/util/BindingAdapters.kt
@@ -1,15 +1,26 @@
 package fr.labard.gpsgpx.util
 
-import android.view.View
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import fr.labard.gpsgpx.R
 import fr.labard.gpsgpx.data.local.RecordEntity
 
-class BindingAdapters {
-    companion object {
-        @BindingAdapter("app:recordName")
-        fun recordName(view: Toolbar, recordEntity: RecordEntity) {
-            view.title = recordEntity.name
+object BindingAdapters {
+    @BindingAdapter("app:recordName")
+    @JvmStatic
+    fun recordName(view: Toolbar, recordEntity: RecordEntity) {
+        view.title = recordEntity.name
+    }
+
+    @BindingAdapter("app:recordingDrawable")
+    @JvmStatic
+    fun recordingDrawable(view: FloatingActionButton, state: String) {
+        if (state == Constants.Service.MODE_RECORD) {
+            view.setImageDrawable(ContextCompat.getDrawable(view.context, R.drawable.ic_baseline_stop_24))
+        } else {
+            view.setImageDrawable(ContextCompat.getDrawable(view.context, R.drawable.ic_baseline_fiber_manual_record_24))
         }
     }
 }
