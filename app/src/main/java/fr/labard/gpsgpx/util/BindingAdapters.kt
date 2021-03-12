d0035a292e1000578c46c3329c918534b0e56227
Antoine Labard <antoine@labard.fr>
2021-03-12 11:25:07 +0100

    Implemented a binding adapter for each of the remaining stat fields.

--- app/src/main/java/fr/labard/gpsgpx/util/BindingAdapters.kt
+++ app/src/main/java/fr/labard/gpsgpx/util/BindingAdapters.kt
@@ -43,4 +43,49 @@ object BindingAdapters {
         val d = LocationOps.getTotalDistance(data)
         view.text = template.format("${d}m")
     }
+
+    @BindingAdapter(value = ["bind:statsTemplate", "bind:totalTime"], requireAll = false)
+    @JvmStatic
+    fun totalTime(view: TextView, template: String, data: List<LocationEntity>) {
+        val t = LocationOps.getTotalTime(data) / 1000
+        val s = t % 60
+        val m = t / 60 % 60
+        val h = t / 3600
+        // the string looks like "00h00m00s"
+        view.text =  template.format((if (h != 0L) "${h}h" else "") + (if (m != 0L) "${m}m" else "") + "${s}s")
+    }
+
+    @BindingAdapter(value = ["bind:statsTemplate", "bind:recentSpeed"], requireAll = false)
+    @JvmStatic
+    fun recentSpeed(view: TextView, template: String, data: List<LocationEntity>) {
+        val s = LocationOps.getRecentSPeed(data)
+        view.text = template.format("${s * 3.6}km/h")
+    }
+
+    @BindingAdapter(value = ["bind:statsTemplate", "bind:averageSpeed"], requireAll = false)
+    @JvmStatic
+    fun averageSpeed(view: TextView, template: String, data: List<LocationEntity>) {
+        val s = LocationOps.getAverageSpeed(data)
+        view.text = template.format("${s * 3.6}km/h")
+    }
+
+    @BindingAdapter(value = ["bind:statsTemplate", "bind:minSpeed"], requireAll = false)
+    @JvmStatic
+    fun minSpeed(view: TextView, template: String, data: List<LocationEntity>) {
+        val s = LocationOps.getMinSpeed(data)
+        view.text = template.format("${s * 3.6}km/h")
+    }
+
+    @BindingAdapter(value = ["bind:statsTemplate", "bind:maxSpeed"], requireAll = false)
+    @JvmStatic
+    fun maxSpeed(view: TextView, template: String, data: List<LocationEntity>) {
+        val s = LocationOps.getMaxSpeed(data)
+        view.text = template.format("${s * 3.6}km/h")
+    }
+
+    @BindingAdapter(value = ["bind:statsTemplate", "bind:nbLocations"], requireAll = false)
+    @JvmStatic
+    fun nbLocations(view: TextView, template: String, data: List<LocationEntity>) {
+    view.text = template.format(LocationOps.getNbLocations(data))
+    }
 }
